4d150b8368aa116d11cf21c0c14cd1b4fcd4307f
irynakrykun <107312287+irynakrykun@users.noreply.github.com>
2022-11-09 14:59:16 +0000

    Deploying to gh-pages from @ irynakrykun/goit-js-hw-09@456450f199bf1777be80c6f8ecea020e568591ee 🚀

---
+++ 01-color-switcher.87f168ec.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]"),n=null;t.addEventListener("click",(function(){n=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.querySelector("body").style.backgroundColor=t}),1e3)})),e.addEventListener("click",(function(){clearInterval(n)}))}();
+//# sourceMappingURL=01-color-switcher.87f168ec.js.map
