456450f199bf1777be80c6f8ecea020e568591ee
irynakrykun <ira_franyk@ukr.net>
2022-11-09 16:58:51 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,19 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+let timerId = null;
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+};
 
+
+startBtn.addEventListener('click', () => {
+  timerId = setInterval(() => {
+        const color = getRandomHexColor();
+  document.querySelector('body').style.backgroundColor = color;
+    }, 1000);
+});
+
+ stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  
+});
